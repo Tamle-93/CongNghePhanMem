21be4488643f0d8d49cd2c1aca23647fb9402c7f
tthhinhpham-pixel <tthhinhpham@gmail.com>
2025-12-13 10:52:16 +0700

    add: mock reviewer assignments api (week 1)

--- Backend/controllers/review_controller.py
+++ Backend/controllers/review_controller.py
@@ -1,3 +1,31 @@
 # File: review_controller.py
 # Nhiệm vụ: Viết code xử lý cho review_controller
 # Team Member: Điền tên người phụ trách vào đây
+# Backend/controllers/review_controller.py
+from flask import Blueprint
+from utils.response import success_response
+
+review_bp = Blueprint("review_bp", __name__, url_prefix="/reviews")
+
+@review_bp.route("/my", methods=["GET"])
+def get_my_reviews():
+    """
+    MOCK DATA - MEMBER 5 - WEEK 1
+    Reviewer: get assigned papers (mock)
+    """
+    mock_data = [
+        {
+            "assignment_id": 1,
+            "paper_title": "AI in Healthcare",
+            "assignment_status": "Assigned",
+            "deadline_date": "2025-01-20"
+        },
+        {
+            "assignment_id": 2,
+            "paper_title": "Smart Traffic Control",
+            "assignment_status": "Assigned",
+            "deadline_date": "2025-01-22"
+        }
+    ]
+
+    return success_response(mock_data, "Mock reviewer assignments - week 1")
